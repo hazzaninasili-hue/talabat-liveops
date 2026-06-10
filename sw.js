5febf27ac87eb29cd09e6d29db148edb0523e474
hazzaninasili-hue <hazzani.nasili@talabat.com>
2026-06-10 16:29:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Talabat Operations - Service Worker
-const CACHE_VERSION = 'talabat-v1781097446';
+const CACHE_VERSION = 'talabat-v1781097886';
 const CACHE_NAME = CACHE_VERSION;
 
 const PRECACHE = ['./', './index.html'];
